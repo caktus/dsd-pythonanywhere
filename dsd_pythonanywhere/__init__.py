da96abc86487609e07d6b01f744e98fe4936379d
Colin Copeland <copelco@caktusgroup.com>
2026-01-04 12:04:37 -0500

    add 0.1.0 to changelog

--- dsd_pythonanywhere/__init__.py
+++ dsd_pythonanywhere/__init__.py
@@ -1,3 +1,8 @@
-__version__ = "0.1.0"
+try:
+    from importlib.metadata import version
+
+    __version__ = version("dsd-pythonanywhere")
+except Exception:
+    __version__ = "unknown"
 
 from .deploy import dsd_deploy, dsd_get_plugin_config  # noqa: F401
